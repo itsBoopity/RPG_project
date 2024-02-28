e3296d889cd9f20ab3d1593fc680efeaf3ac51d5
Boopity <itsboopity@gmail.com>
2024-02-28 12:00:28 +0100

    Prepared localization file and added animation to InfoLabel using anchor points instead of position

--- src/UI/Battle/InfoLabel.cs
+++ src/UI/Battle/InfoLabel.cs
@@ -6,8 +6,11 @@ public partial class InfoLabel : Node
     private TextureRect icon;
     private RichTextLabel label;
     private AnimationPlayer animationPlayer;
+
+    private ControlState currentState = ControlState.FULLY_DISABLED; 
     public override void _Ready()
     {
+        TranslationServer.SetLocale("en");
         icon = GetNode<TextureRect>("Icon");   
         label = GetNode<RichTextLabel>("Label");
         animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
@@ -15,26 +18,38 @@ public partial class InfoLabel : Node
 
     public void UpdateUI(ControlState state)
     {
+        GD.Print(Tr("T_B_IL_PT"));
         if (state == ControlState.PLAYER_DEFAULT)
         {
-            label.Text = "Player Turn";
+            label.Text = "T_B_IL_PT";
             icon.Texture = GD.Load<Texture2D>("res://Images/UI/Battle/InfoLabel/player.png");
+            if (currentState == ControlState.ENEMY_TURN)
+            {
+                animationPlayer.Play("Slide");
+            }
+            else
+            {
+                animationPlayer.Play("Swap");
+            }
         }
         else if (state == ControlState.PLAYER_TARGETTING_ENEMY)
         {
-            label.Text = "Enemy Targetting";
+            label.Text = "T_B_IL_ET";
             icon.Texture = GD.Load<Texture2D>("res://Images/UI/Battle/InfoLabel/targetting.png");
+            animationPlayer.Play("Swap");
         }
         else if (state == ControlState.PLAYER_TARGETTING_ALLY)
         {
-            label.Text = "Ally Targetting";
+            label.Text = "T_B_IL_AT";
             icon.Texture = GD.Load<Texture2D>("res://Images/UI/Battle/InfoLabel/ally.png");
+            animationPlayer.Play("Swap");
         }
         else if (state == ControlState.ENEMY_TURN)
         {
-            label.Text = "Enemy Turn";
+            label.Text = "T_B_IL_ETU";
             icon.Texture = GD.Load<Texture2D>("res://Images/UI/Battle/InfoLabel/enemy.png");
+            animationPlayer.Play("Slide");
         }
-        animationPlayer.Play("Swap");
+        currentState = state;
     }
 }
